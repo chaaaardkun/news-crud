b267b884d431674f61e2a4866dbe02d53b316fca
chad <chaaaardkun@gmail.com>
2022-06-08 20:43:25 +0800

    :wrench: update prettier config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
-import "@/assets/styles/styles.scss";
+import { RouterView } from 'vue-router';
+import '@/assets/styles/styles.scss';
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo m-auto mt-5"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <img alt="Vue logo" class="logo m-auto mt-5" src="@/assets/logo.svg" width="125" height="125" />
   </header>
 
   <RouterView />
